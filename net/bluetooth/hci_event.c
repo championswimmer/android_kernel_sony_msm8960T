94968f3f60f1a6836fac2c75cf93110ab89f5fe0
Srinivas Krovvidi <skrovvid@codeaurora.org>
2012-08-24 13:25:18 +0530

    Bluetooth: Avoid re-authentication for secure high connections
    The re-authentication code changes are added to mandate 16 digit
    PIN Key for SECURE_HIGH connections. But only SAP profile needs
    16 digit key, not all the profiles that register with SECURITY
    HIGH. Avoid the code to mandate 16 digit key.
    
    (cherry picked from commit 087e3a61334b4bdb03e13e0bfcb535517840526c)
    CRs-Fixed: 387173
    Change-Id: I960b01f873e21296c91f1e5e64ba317b0bd68002
    Signed-off-by: Srinivas Krovvidi <skrovvid@codeaurora.org>
    Signed-off-by: Ram Mohan Korukonda <rkorukon@codeaurora.org>
    Signed-off-by: Dhivya Subramanian <dthiru@codeaurora.org>
    
    Conflicts:
    	net/bluetooth/hci_event.c

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -1,7 +1,6 @@
 /*
    BlueZ - Bluetooth protocol stack for Linux
-   Copyright (c) 2000-2001, 2010-2012, Code Aurora Forum. All rights reserved.
-   Copyright (C) 2012 Sony Mobile Communications AB.
+   Copyright (c) 2000-2001, 2010-2012 The Linux Foundation. All rights reserved.
 
    Written 2000,2001 by Maxim Krasnyansky <maxk@qualcomm.com>
 
@@ -1841,6 +1840,15 @@ static inline void hci_auth_complete_evt(struct hci_dev *hdev, struct sk_buff *s
 			struct hci_cp_auth_requested cp;
 			hci_remove_link_key(hdev, &conn->dst);
 			cp.handle = cpu_to_le16(conn->handle);
+			/*Initiates dedicated bonding as pin or key is missing
+			on remote device*/
+			/*In case if remote device is ssp supported,
+			reduce the security level to MEDIUM if it is HIGH*/
+			if (conn->ssp_mode && conn->auth_initiator &&
+				conn->io_capability != 0x03) {
+				conn->pending_sec_level = BT_SECURITY_HIGH;
+				conn->auth_type = HCI_AT_DEDICATED_BONDING_MITM;
+			}
 			hci_send_cmd(conn->hdev, HCI_OP_AUTH_REQUESTED,
 							sizeof(cp), &cp);
 			hci_dev_unlock(hdev);
@@ -2667,12 +2675,6 @@ static inline void hci_link_key_request_evt(struct hci_dev *hdev, struct sk_buff
 	if (conn) {
 		BT_DBG("Conn pending sec level is %d, ssp is %d, key len is %d",
 			conn->pending_sec_level, conn->ssp_mode, key->pin_len);
-	}
-	if (conn && (conn->ssp_mode == 0) &&
-		(conn->pending_sec_level == BT_SECURITY_HIGH) &&
-		(key->pin_len != 16)) {
-		BT_DBG("Security is high ignoring this key");
-		goto not_found;
 	}
 
 	if (key->key_type == 0x04 && conn && conn->auth_type != 0xff &&
